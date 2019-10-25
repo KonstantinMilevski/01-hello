6fd3a988a39f120039cb42f3fe34060b7f92875b
KonstantinMilevski <milevsky.k@gmail.com>
2019-10-25 12:41:17 +0300

    adding files

---
+++ hello_lib/src/print.cpp
@@ -0,0 +1,6 @@
+#include "print.h"
+bool print()
+{
+    std::cout << "Hello, world" << std::endl;
+    return std::cout.good();
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "print.h"
+#include <cstdlib>
+int main()
+{
+    int rezult = print();
+    return rezult ? EXIT_SUCCESS : EXIT_FAILURE;
+}
