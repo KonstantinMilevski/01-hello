27255457fd2eedcb3cdcc752b9adbf5c349b2853
KonstantinMilevski <milevsky.k@gmail.com>
2019-11-30 16:46:19 +0300

    add_05-1

---
+++ 05-1-vertex-morphing/engine.hxx
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <iosfwd>
+#include <string>
+#include <string_view>
+#include <vector>
+
+struct vertex
+{
+    vertex()
+        : x(0.0f)
+        , y(0.0f)
+    {
+    }
+    float x;
+    float y;
+};
+
+struct triangle
+{
+    triangle()
+    {
+        v[0] = vertex();
+        v[1] = vertex();
+        v[2] = vertex();
+    }
+    vertex v[3];
+};
+
+std::istream& operator>>(std::istream& is, vertex& v);
+std::istream& operator>>(std::istream& is, triangle& t);
+
+class engine;
+
+engine* create_engine();
+void    destroy_engine(engine*);
+
+class engine
+{
+public:
+    ~engine();
+    virtual std::string initialize(std::string_view)     = 0;
+    virtual void        uninitialize()                   = 0;
+    virtual void        render_triangle(const triangle&) = 0;
+    virtual void        swap_buffer()                    = 0;
+    /// execute while "close SDLevent"
+    virtual bool read_input(int&) = 0;
+    /// return seconds from initialization
+    virtual float get_time_from_init() = 0;
+};
---
+++ 05-1-vertex-morphing/main.cxx
@@ -0,0 +1,76 @@
+#include "engine.hxx"
+#include <cassert>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <memory>
+
+vertex blend_vertex(const vertex& left, const vertex& right, const float& t)
+{
+    vertex v;
+    v.x = left.x + t * (right.x - left.x);
+    v.y = left.y + t * (right.y - left.y);
+    return v;
+}
+
+triangle blend(const triangle& left, const triangle& right, const float& t)
+{
+
+    triangle tr;
+    tr.v[0] = blend_vertex(left.v[0], right.v[0], t);
+    tr.v[1] = blend_vertex(left.v[1], right.v[1], t);
+    tr.v[2] = blend_vertex(left.v[2], right.v[2], t);
+    return tr;
+}
+
+using namespace std;
+
+int main()
+{
+    std::unique_ptr<engine, void (*)(engine*)> engine(create_engine(),
+                                                      destroy_engine);
+
+    const std::string error = engine->initialize("");
+    if (!error.empty())
+    {
+        std::cerr << error << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::ifstream file("vertexes.txt");
+    assert(!!file);
+    triangle t1, t2, t3, t4;
+
+    file >> t1 >> t2 >> t3 >> t4;
+
+    bool continue_loop = true;
+    while (continue_loop)
+    {
+        int key = 0;
+
+        while (engine->read_input(key))
+        {
+            switch (key)
+            {
+                case 0:
+                    continue_loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        float alpha = (std::sin(engine->get_time_from_init()) * 0.5f) + 0.5f;
+
+        // triangle tr1 = blend(t1, t3, alpha);
+        triangle tr2 = blend(t2, t4, alpha);
+
+        // engine->render_triangle(tr1);
+        engine->render_triangle(tr2);
+
+        engine->swap_buffer();
+    };
+
+    engine->uninitialize();
+    return 0;
+}
