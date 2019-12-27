4726bdcad21b43e13784dd6d8a776e70f46d300b
KonstantinMilevski <milevsky.k@gmail.com>
2019-12-27 21:27:52 +0300

    work...1_fig

--- try_tet/main.cxx
+++ try_tet/main.cxx
@@ -29,38 +29,35 @@ static const std::vector<vertex> figures_coord = {
     { 0.0, -0.5, 0.0, 0.0 },
     { 0.0, -1.5, 0.0, 0.0 }, // I
     //
-    { 0.0, 1.5, 0.0, 0.0 },
-    { 0.0, 0.5, 0.0, 0.0 },
-    { -1.0, 0.5, 0.0, 0.0 },
-    { -1.0, -0.5, 0.0, 0.0 }, // Z
+    { 0.5, 1.0, 0.0, 0.0 },
+    { 0.5, 0.0, 0.0, 0.0 },
+    { -0.5, 0.0, 0.0, 0.0 },
+    { -0.5, -1.0, 0.0, 0.0 }, // Z
 
-    //    { -1.0, -0.5, 0.0, 0.0 }, { -1.0, 0.5, 0.0, 0.0 },
-    //    { 0.0, -0.5, 0.0, 0.0 },  { 0.0, 0.5, 0.0, 0.0 }, // S
+    { 0.5, -1.5, 0.0, 0.0 },
+    { 0.5, -0.5, 0.0, 0.0 },
+    { -0.5, -0.5, 0.0, 0.0 },
+    { -0.5, 0.5, 0.0, 0.0 }, // S ??
 
-    { 0.0, -0.5, 0.0, 0.0 },
-    { 0.0, 0.5, 0.0, 0.0 },
-    { -1.0, 0.5, 0.0, 0.0 },
-    { -1.0, 1.5, 0.0, 0.0 }, // S
+    { -0.5, -1.0, 0.0, 0.0 },
+    { -0.5, 0.0, 0.0, 0.0 },
+    { -0.5, 1.0, 0.0, 0.0 },
+    { 0.5, 0.0, 0.0, 0.0 }, // T
 
-    { -1.0, -0.5, 0.0, 0.0 },
-    { -1.0, 0.5, 0.0, 0.0 },
-    { -1.0, 1.5, 0.0, 0.0 },
-    { 0.0, 0.5, 0.0, 0.0 }, // T
+    { -0.5, 1.0, 0.0, 0.0 },
+    { -0.5, 0.0, 0.0, 0.0 },
+    { -0.5, -1.0, 0.0, 0.0 },
+    { 0.5, -1.0, 0.0, 0.0 }, // L
 
-    { -1.0, 1.5, 0.0, 0.0 },
-    { -1.0, 0.5, 0.0, 0.0 },
-    { -1.0, -0.5, 0.0, 0.0 },
-    { 0.0, -0.5, 0.0, 0.0 }, // L
+    { 0.5, 1.0, 0.0, 0.0 },
+    { 0.5, 0.0, 0.0, 0.0 },
+    { 0.5, -1.0, 0.0, 0.0 },
+    { -0.5, -1.0, 0.0, 0.0 }, // J
 
-    { 0.0, 1.5, 0.0, 0.0 },
-    { 0.0, 0.5, 0.0, 0.0 },
-    { 0.0, -0.5, 0.0, 0.0 },
-    { -1.0, -0.5, 0.0, 0.0 }, // J
-
-    { -1.0, 1.0, 0.0, 0.0 },
-    { -1.0, 0.0, 0.0, 0.0 },
-    { 0.0, 1.0, 0.0, 0.0 },
-    { 0.0, 0.0, 0.0, 0.0 } // O
+    { -0.5, 0.5, 0.0, 0.0 },
+    { -0.5, -0.5, 0.0, 0.0 },
+    { 0.5, 0.5, 0.0, 0.0 },
+    { 0.5, -0.5, 0.0, 0.0 } // O
 };
 
 /// global
@@ -108,15 +105,16 @@ struct block
 {
     block() = default;
 
-    block(vertex pos, float size, rect uv_pos, texture* tex)
+    block(vertex pos, /*float size,*/ rect uv_pos, texture* tex)
         : centr_pos_(pos)
-        , size_(size)
+        //, size_(size)
         , rect_pos_(uv_pos)
         , tex_(tex)
     {
     }
     std::vector<tri2> build_block()
     {
+        float                 size_ = 1.f;
         std::array<vertex, 4> quad;
         // you can change colore of texture with shift uv coord
         ///   0            1
@@ -152,9 +150,11 @@ struct block
 
         for (auto& qu : quad)
         {
-            /// scale window
-            // qu.pos *= { static_cast<float>(screen_height) / screen_width, 1.f
-            // };
+            /// scale to GL_window
+            qu.pos /= 2.f;
+            /// scale to monitor_window
+            qu.pos *= { static_cast<float>(screen_height) / screen_width, 1.f };
+            qu.pos /= 5;
         }
 
         tri2              t0(quad[3], quad[1], quad[2]);
@@ -165,20 +165,19 @@ struct block
         return quad_tri;
     }
 
-    vertex   centr_pos_;     /// centr of quadrate
-    float    size_;          /// size of quadrate
+    vertex centr_pos_; /// centr of cell
+    // float    size_;          /// size of cell
     rect     rect_pos_;      /// position of texture
-    texture* tex_ = nullptr; /// texture for quadrate
+    texture* tex_ = nullptr; /// texture for cell
 };
 
 struct figure
 {
     // figure() {}
-    figure(block c, std::array<vertex, 4>& arr, vec2 pos)
+    figure(block c, std::vector<vertex>& arr, vec2 pos)
         : cell(c)
         , pos{ arr }
         , fig_center(pos)
-
     {
     }
 
@@ -189,61 +188,16 @@ struct figure
 
         for (int i = 0; i < 4; i++)
         {
-            block             b(pos[i], cell.size_, cell.rect_pos_, cell.tex_);
+            block             b(pos[i], cell.rect_pos_, cell.tex_);
             std::vector<tri2> res = b.build_block();
-            vec_tr.push_back(res[1]);
-            vec_tr.push_back(res[2]);
-            // std::copy_n(begin(res), 2, begin(vec_tr));
+            std::copy_n(begin(res), 2, std::back_inserter(vec_tr));
         }
-        //        //        block b00(pos[0], cell.size_, cell.rect_pos_,
-        //        cell.tex_);
-        //        //        block b01(pos[1], cell.size_, cell.rect_pos_,
-        //        cell.tex_);
-        //        //        block b02(pos[2], cell.size_, cell.rect_pos_,
-        //        cell.tex_);
-        //        //        block b03(pos[3], cell.size_, cell.rect_pos_,
-        //        cell.tex_);
-        //        // std::vector<tri2> vec_tr;
 
-        //        float size_uv_x = 1 / 7.f; /// static_cast<float>(width);
-        //        float size_uv_y = 1;       /// static_cast<float>(heigh);
-        //        float step      = 1.f;
-
-        //        /// remember in OpenGL texture lower left angle is (0, 0)
-        //        coordinate for (int i = 0; i < 4; i++)
-        //        {
-
-        //            std::array<vertex, 4> vertexes;
-        //            vertexes[0].pos = { fig_center.x + pos[i].pos.x,
-        //                                fig_center.y + pos[i].pos.y };
-        //            vertexes[1].pos = { fig_center.x + pos[i].pos.x + step,
-        //                                fig_center.y + pos[i].pos.y };
-        //            vertexes[2].pos = { fig_center.x + pos[i].pos.x + step,
-        //                                fig_center.y + pos[i].pos.y - step };
-        //            vertexes[3].pos = { fig_center.x + pos[i].pos.x,
-        //                                fig_center.y + pos[i].pos.y - step };
-
-        //            vertexes[0].uv = { pos[i].uv.x, pos[i].uv.y + size_uv_y };
-        //            vertexes[1].uv = { pos[i].uv.x + size_uv_x,
-        //                               pos[i].uv.y + size_uv_y };
-        //            vertexes[2].uv = { pos[i].uv.x + size_uv_x, pos[i].uv.y };
-        //            vertexes[3].uv = { pos[i].uv.x, pos[i].uv.y };
-
-        //            // to GL coord, scale 2;
-        //            for (auto& v : vertexes)
-        //            {
-        //                v.pos /= 2.f;
-        //            }
-        //            tri2 t0(vertexes[3], vertexes[1], vertexes[2]);
-        //            tri2 t1(vertexes[0], vertexes[3], vertexes[1]);
-        //            vec_tr.push_back(t0);
-        //            vec_tr.push_back(t1);
-        //        }
         return vec_tr;
     }
-    block                 cell;       /// one block of figure
-    vec2                  fig_center; /// center of all figure
-    std::array<vertex, 4> pos;        /// position of cell for figure
+    block               cell;       /// one block of figure
+    vec2                fig_center; /// center of all figure
+    std::vector<vertex> pos;        /// position of cell for figure
 };
 
 int main()
@@ -272,7 +226,7 @@ int main()
     // block
     vertex            f_pos{ -0.0, -0.0, 0.0, 0.0 };
     rect              r{ { 0, 0 }, { 1.f / 7.f, 1 } };
-    block             f(f_pos, 0.1, r, texture);
+    block             f(f_pos, r, texture);
     std::vector<tri2> t_res = f.build_block();
     vertex_buffer*    one_quad_buff =
         engine->create_vertex_buffer(&t_res[0], t_res.size());
@@ -280,16 +234,15 @@ int main()
     // end_block
 
     // figure
-    vec2                  central_pos(0.5f, 0.5f);
-    std::array<vertex, 4> res1;
-    for (int i = 0; i < 4; i++)
-    {
-        res1[i] = figures_coord[i];
-    }
+    vec2                central_pos(0.5f, 0.5f);
+    std::vector<vertex> res1;
+
+    std::copy_n(begin(figures_coord) + n, 4, std::back_inserter(res1));
+
     figure fig_I(f, res1, central_pos);
 
-    std::vector<tri2> res;
-    res = fig_I.build_all_tr();
+    std::vector<tri2> res{ fig_I.build_all_tr() };
+
     vertex_buffer* vert_buff =
         engine->create_vertex_buffer(&res[0], res.size());
 
@@ -313,62 +266,57 @@ int main()
                 case keys::exit:
                     continue_loop = false;
                     break;
-                    //                case keys::right:
-                    //                    if (engine->is_key_down(keys::right))
-                    //                    {
-                    //                        std::cout << "keys::right" <<
-                    //                        std::endl; current_pos.x += 0.05f;
-                    //                        check_border(current_pos.x);
-                    //                        check = false;
-                    //                        break;
+                case keys::right:
+                    if (engine->is_key_down(keys::right))
+                    {
+                        std::cout << "keys::right" << std::endl;
+                        current_pos.x += 0.05f;
+                        // check_border(current_pos.x);
+                        check = false;
+                        break;
 
-                    //                        // game_event.is_down == false;
-                    //                    }
-                    //                case keys::left:
-                    //                    if (engine->is_key_down(keys::left))
-                    //                    {
-                    //                        std::cout << "keys::left" <<
-                    //                        std::endl; current_pos.x -= 0.05f;
-                    //                        check_border(current_pos.x);
-                    //                        check = false;
-                    //                        break;
-                    //                    }
-                    //                case keys::rotate:
-                    //                    if (engine->is_key_down(keys::rotate))
-                    //                    {
-                    //                        std::cout << "keys::rotate" <<
-                    //                        std::endl; current_direction += pi
-                    //                        / 2.f;
-                    //                        //                        if
-                    //                        // (!(std::fmod(current_direction,
-                    //                        //                        pi) <
-                    //                        0.01f))
-                    //                        // current_scale =
-                    //                        // static_cast<float>(heigh)
-                    //                        //                            /
-                    //                        // static_cast<float>(width);
-                    //                        //                        else
-                    //                        // current_scale = 1.f; check =
-                    //                        false; break;
-                    //                        // current_direction = pi / 2.f;
-                    //                    }
+                        // game_event.is_down == false;
+                    }
+                case keys::left:
+                    if (engine->is_key_down(keys::left))
+                    {
+                        std::cout << "keys::left" << std::endl;
+                        current_pos.x -= 0.05f;
+                        // check_border(current_pos.x);
+                        check = false;
+                        break;
+                    }
+                case keys::rotate:
+                    if (engine->is_key_down(keys::rotate))
+                    {
+                        std::cout << "keys::rotate" << std::endl;
+                        current_direction += pi / 2.f;
+                        if (!(std::fmod(current_direction, pi) < 0.01f))
+                            current_scale = static_cast<float>(screen_height) /
+                                            static_cast<float>(screen_width);
+                        else
+                            current_scale = 1.f;
+                        check = false;
+                        break;
+                        // current_direction = pi / 2.f;
+                    }
                 default:
                     break;
             }
         }
-        vec2   centr_pos(0.0f, -0.9f);
+        vec2   centr_pos(0.0f, -0.0f);
         matrix move_cen = matrix::move(centr_pos);
-        vec2   back_pos(0.0f, 0.9f);
+        vec2   back_pos(0.0f, 0.0f);
         matrix move_back = matrix::move(back_pos);
         matrix move      = matrix::move(current_pos);
-        matrix aspect    = matrix::scale(1, current_scale);
+        matrix aspect    = matrix::scale(current_scale, 1);
         matrix rot       = matrix::rotation(current_direction);
         matrix m         = move_cen * rot * move_back * move * aspect;
 
-        engine->render_tetris(*one_quad_buff, f.tex_);
+        // engine->render_tetris(*one_quad_buff, f.tex_);
 
         // engine->render_tetris(*vert_buff, fig_I.cell.tex_);
-        // engine->render_tet(*vert_buff, texture, m);
+        engine->render_tet(*vert_buff, texture, m);
 
         engine->swap_buffer();
     }
